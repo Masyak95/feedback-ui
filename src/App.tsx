fd5ac6f80cf2fe49e9d85d2bb03f3f2376b24a2a
Maryia <97767940+Masyak95@users.noreply.github.com>
2023-08-31 14:00:41 +0200

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import Header from './components/Header/Header';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Header from './components/Header';
 import feedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import {FeedbackType} from "./components/FeedbackItem";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
 
 function App() {
     const [feedback, setFeedback] = useState<FeedbackType[]>(feedbackData);
@@ -22,15 +25,26 @@ function App() {
     };
 
     return (
-        <div>
+        <Router>
             <Header text={'Feedback UI'} />
-            <FeedbackForm handleAdd={addFeedback} />
-            <FeedbackStats feedback={feedback} />
-            <FeedbackList
-                feedback={feedback}
-                handleDelete={deleteFeedback}
-            />
-        </div>
+            <div className="container">
+                <Routes>
+                    <Route path={'/'} element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback} />
+                            <FeedbackStats feedback={feedback} />
+                            <FeedbackList
+                                feedback={feedback}
+                                handleDelete={deleteFeedback}
+                            />
+                        </>
+                    }>
+                    </Route>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                </Routes>
+            </div>
+                <AboutIconLink/>
+        </Router>
     );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+type HeaderPropsType = {
+    text: string;
+    bgColor: string;
+    textColor: string;
+}
+
+const Header = (props: HeaderPropsType) => {
+    const {text, textColor, bgColor}= props
+
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor,
+    }
+
+    return (
+        <header style={headerStyles}>
+            <div className={"container"}>
+                <h2>{text}</h2>
+            </div>
+        </header>
+    );
+};
+
+Header.defaultProps = {
+    text: 'Feedback UI',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95',
+}
+export default Header;
